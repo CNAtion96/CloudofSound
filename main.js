005525db93783e403518f5ce8da9850a36a57d9f
Chris Adkins <c.adkins19967@gmail.com>
2017-03-22 10:04:31 -0400

    updated js

--- main.js
+++ main.js
@@ -12,20 +12,19 @@ $('#artist').submit(function (e) {
             console.log(responce);
             responce.forEach(function (song) {
                 var sId = song.id;
-                songURL = 'http://api.soundcloud.com/tracks/' + sId + '/stream?client_id=03e4633e2d85874a921380e47cac705d';
                 if (song.artwork_url === null) {
-                    $('#artistOutput').append("<div class=\"col-md-3 song\">\n                            <div class=\"songImage\" style=\"background-image: url(not-found.jpg);\">\n                                <div class=\"play\"><img src=\"arrow.svg\"></div>\n                            </div>\n                            <p>" + song.title + "<p>\n                        </div>");
+                    $('#artistOutput').append("<div class=\"col-md-3 song\">\n                            <div class=\"songImage\" style=\"background-image: url(not-found.jpg);\">\n                                <div class=\"play\" data-id=\"" + sId + "\"><img src=\"arrow.svg\"></div>\n                            </div>\n                            <p>" + song.title + "<p>\n                        </div>");
                 }
                 else {
-                    $('#artistOutput').append("<div class=\"col-md-3 song\">\n                            <div class=\"songImage\" style=\"background-image: url(" + song.artwork_url + ")\">\n                                <div class=\"play\"><img src=\"arrow.svg\"></div>\n                            </div>\n                            <p>" + song.title + "<p>\n                        </div>");
+                    $('#artistOutput').append("<div class=\"col-md-3 song\">\n                            <div class=\"songImage\"  style=\"background-image: url(" + song.artwork_url + ")\">\n                                <div class=\"play\" data-id=\"" + sId + "\"><img src=\"arrow.svg\"></div>\n                            </div>\n                            <p>" + song.title + "<p>\n                        </div>");
                 }
-                return songURL;
             });
         }
     });
 });
 $('body').on('click', '.play', function () {
-    console.log(songURL);
     var id = $(this).data('id');
-    $('#audio').attr('src', songURL);
+    console.log(id);
+    var urlSource = "http://api.soundcloud.com/tracks/" + id + "/stream?client_id=03e4633e2d85874a921380e47cac705d";
+    $('#audio').attr('src', urlSource);
 });
